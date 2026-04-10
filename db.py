4bd264c8b7f963ba798b610ddf02062772f53db2
Aadrika12323 <156059858+Aadrika12323@users.noreply.github.com>
2026-04-10 20:12:26 +0530

    Update db.py

--- db.py
+++ db.py
@@ -18,7 +18,7 @@ else:
     conn = mysql.connector.connect(
         host="localhost",
         user="root",
-        password="password",
+        password=os.getenv("DB_PASSWORD"),
         database="sentinel_aiops"
     )
     cursor = conn.cursor()
