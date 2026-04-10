2638a10361f51b58ceec6f4c70f0b49be354112d
Aadrika12323 <156059858+Aadrika12323@users.noreply.github.com>
2026-04-10 19:20:08 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,9 +1,24 @@
-import mysql.connector
 import os
 
-conn = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password=os.getenv("DB_PASSWORD"),
-    database="sentinel_aiops"
-)
+if os.getenv("ENV") == "production":
+    import sqlite3
+    conn = sqlite3.connect("logs.db", check_same_thread=False)
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS logs (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        log TEXT,
+        solution TEXT,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+    """)
+else:
+    import mysql.connector
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password=os.getenv("DB_PASSWORD"),
+        database="sentinel_aiops"
+    )
+    cursor = conn.cursor()
